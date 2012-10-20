147bbd61cc9fd1e7ddc02406e4fbcc5d44daa904
Brennan Meadowcroft <brennan@brennanmeadowcroft.com>
2012-10-20 10:41:09 -0600

    Add position name on experience form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
 
-#  unless Rails.application.config.consider_all_requests_local
+  unless Rails.application.config.consider_all_requests_local
     rescue_from Exception, with: :render_500
     rescue_from ActionController::RoutingError, with: :render_404
     rescue_from ActionController::UnknownController, with: :render_404
     rescue_from ActionController::UnknownAction, with: :render_404
     rescue_from ActiveRecord::RecordNotFound, with: :render_404
-#  end
+  end
 
   private
   def render_404(exception)
